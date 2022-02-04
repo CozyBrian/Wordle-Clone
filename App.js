c2d66476756585b903eaf95ba5129c9e4cf0b88a
CozyBrian <dragonbrian33@gmail.com>
2022-02-04 23:49:22 +0000

    Keyboard and Cells

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import { colors } from './src/constants';
+import { SafeArea } from './src/utils/SafeArea';
+import Keyboard from './src/components/Keyboard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeArea style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Text style={styles.title}>WORDLE</Text>
+
+      <View style={styles.map}>
+        <View style={styles.row}>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+        </View>
+      </View>
+
+      <Keyboard/>
+    </SafeArea>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.black,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: "bold",
+    letterSpacing: 7,
+  },
+  map: { 
+    alignSelf: "stretch",
+    height: 100
+  },
+  row: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+  },
+  cell: {
+    flex: 1,
+    height: 30,
+    borderColor: colors.darkgrey,
+    borderWidth: 2,
+    aspectRatio: 1,
+    margin: 5,
+    
   },
 });
---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const colors = {
+ black: "#121214",
+ darkgrey: "#3a3a3d",
+ lightgrey: "#818384",
+ primary: "#538d4e",
+ secondary: "#b59f3b"
+}
+
+export const colorsToEmoji = {
+
+}
+
+export const ENTER = "ENTER";
+export const CLEAR = "CLEAR";
+
+export const keys = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  [ENTER, "z", "x", "c", "v", "b", "n", "m", CLEAR],
+]
