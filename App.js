e115cb1787b17ec57b1445551b15e60aa9439e63
CozyBrian <dragonbrian33@gmail.com>
2022-02-05 00:17:33 +0000

    Some logic

--- App.js
+++ App.js
@@ -4,20 +4,30 @@ import { colors } from './src/constants';
 import { SafeArea } from './src/utils/SafeArea';
 import Keyboard from './src/components/Keyboard';
 
+const NUMBER_OF_TRIES = 6;
+
 export default function App() {
+  const word = "hello";
+  const letters = word.split('');
+
+  const rows = new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill('a'));
+
   return (
     <SafeArea style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.title}>WORDLE</Text>
 
       <View style={styles.map}>
-        <View style={styles.row}>
-          <View style={styles.cell}/>
-          <View style={styles.cell}/>
-          <View style={styles.cell}/>
-          <View style={styles.cell}/>
-          <View style={styles.cell}/>
-        </View>
+
+        {rows.map((row) => (
+          <View style={styles.row}>
+            {row.map((cell) => (
+            <View style={styles.cell}>
+              <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+            </View>
+            ))}      
+          </View>
+        ))}
       </View>
 
       <Keyboard/>
@@ -44,14 +54,21 @@ const styles = StyleSheet.create({
   row: {
     alignSelf: "stretch",
     flexDirection: "row",
+    justifyContent: "center"
   },
   cell: {
     flex: 1,
-    height: 30,
     borderColor: colors.darkgrey,
     borderWidth: 2,
     aspectRatio: 1,
-    margin: 5,
-    
+    margin: 3,
+    maxWidth: 80,
+    justifyContent: "center",
+    alignItems: "center"
   },
+  cellText: {
+    color: colors.lightgrey,
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
